e4b3dd146845540e62a90a35621fd67f0805ce4f
Gabriele Lauricella <gab.lau@gmail.com>
2025-07-12 08:51:34 +0200

    new rpi models + update dependancies

--- rpi-generator.js
+++ rpi-generator.js
@@ -3,23 +3,18 @@ Extract RPI model from offical site and generate pi-model.json file
 */
 
 const fetch = require('node-fetch');
-const DomParser = require('dom-parser');
 const fs = require('fs');
 
 console.log('Extracting RPI model from: ');
-const rpiRevisionCodeUrl = 'https://www.raspberrypi.org/documentation/hardware/raspberrypi/revision-codes/README.md';
+const rpiRevisionCodeUrl = 'https://github.com/raspberrypi/documentation/raw/refs/heads/develop/documentation/asciidoc/computers/raspberry-pi/revision-codes.adoc';
 console.log(rpiRevisionCodeUrl);
 
 fetch(rpiRevisionCodeUrl)
     .then(function (response) {
         return response.text();
     })
-    .then(function (html) {
-        const parser = new DomParser();
-        const doc = parser.parseFromString(html, "text/html");
-        const tables = doc.getElementsByTagName('table');
-        var rows = null;
-        var tds = null;
+    .then(async function (markdown) {
+        const lines = markdown.split("\n");
 
         var allmodel = [
             {
@@ -29,35 +24,52 @@ fetch(rpiRevisionCodeUrl)
             },
         ];
 
-        //convert old revision code
-        rows = tables[0].getElementsByTagName('tr');
-        for (i = 0; i < rows.length; i++) {
-            var tds = rows[i].getElementsByTagName('td');
-            if (tds.length <= 0) continue;
-            jsrow = {
-                "Code": tds[0].innerHTML,
-                "Model": tds[1].innerHTML,
-                "Revision": tds[2].innerHTML,
-                "RAM": tds[3].innerHTML,
-                "Manufacturer": tds[4].innerHTML,
-            };
-            allmodel.push(jsrow);
+        let start = false;
+        let table = "";
+        let header = "";
+        for (i = 0; i < lines.length; i++) {
+            const line = lines[i];
+
+            if (!start) {
+                table = "";
+                header = "";
+            }
+            if (line.trim().startsWith("|==")) {
+                if (!start) {
+                    header = lines[i + 1];
+                    start = true;
+                    continue;
+                } else {
+                    start = false;
+
+                    table = table.replaceAll("\n|", " |");
+                    const ls = table.split("\n");
+
+                    let ck = ls[0].replaceAll(" ", "").trim();
+                    if (!(ck == "|Code|Model|Revision|RAM|Manufacturer")) continue;
+
+                    for (j = 0; j < ls.length; j++) {
+                        if (j == 0) continue;
+
+                        let l = ls[j].trim().substring(1).split("|");
+                        if (l.length < 5) continue;
+                        let jsrow = {
+                            "Code": l[0].trim(),
+                            "Model": l[1].trim(),
+                            "Revision": l[2].trim(),
+                            "RAM": l[3].trim(),
+                            "Manufacturer": l[4].trim(),
+                        };
+                        allmodel.push(jsrow);
+                    }
+                }
+            }
+            if (start) {
+                table = table + line + "\n";
+            }
         }
 
-        //convert new revision code
-        rows = tables[2].getElementsByTagName('tr');
-        for (i = 0; i < rows.length; i++) {
-            tds = rows[i].getElementsByTagName('td');
-            if (tds.length <= 0) continue;
-            jsrow = {
-                "Code": tds[0].innerHTML,
-                "Model": tds[1].innerHTML,
-                "Revision": tds[2].innerHTML,
-                "RAM": tds[3].innerHTML,
-                "Manufacturer": tds[4].innerHTML,
-            };
-            allmodel.push(jsrow);
-        }
+
         console.log('Found ' + (allmodel.length - 1) + ' different models! ');
         console.log('Saving pi-model.json ... ');
         var data = JSON.stringify(allmodel, null, 2);
